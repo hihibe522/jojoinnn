3474e4175f8c0b14d17ae6cbcfed6b9f5e7999e3
Be <hihibe@gmail.com>
2020-08-09 20:42:51 +0800

    0809_4

--- server/routes/payPage.js
+++ server/routes/payPage.js
@@ -3,11 +3,12 @@ var router = express.Router();
 var conn = require("../db");
 const { request } = require("express");
 
-var activityID = 50;
-var m_ID = 5;
-var m_name = "fk";
+var activityID;
+// var m_ID = 5;
+// var m_name = "fk";
 var dataList={};
-router.get("/", function(req, res, next) {
+router.get("/:actID", function(req, res, next) {
+  activityID= req.params.actID;
   conn.query(
     `SELECT * from current_activity  where a_ID=${activityID}`,
     function(err, rows) {
@@ -15,33 +16,22 @@ router.get("/", function(req, res, next) {
         // console.log(JSON.stringify(err));
         return;
       }
-      console.log(rows)
+      // console.log(rows)
       dataList.payment=rows;
-      next();
-    }
-  )
-})
-router.get("/", function(req, res, next) {
-  conn.query(
-    `SELECT m_ID,m_name,joCoin from member  where m_ID=${m_ID}`,
-    function(err, rows) {
-      if (err) {
-        // console.log(JSON.stringify(err));
-        return;
-      }
-      console.log(rows)
-      dataList.member=rows;
       res.send(dataList);
-
     }
   )
 })
+
 
 
 
 router.put("/", function(req, res, next) {
+  // console.log(activityID);
+  // console.log(req.body)
+  // console.log(req.body.m_ID)
   conn.query(
-    `UPDATE transaction SET m_pay=1 WHERE m_ID=${m_ID} and a_ID=${activityID}`,
+    `UPDATE transaction SET m_pay=1 WHERE m_ID=${req.body.m_ID} and a_ID=${activityID}`,
     function(err, row) {
       if (err) {
         console.log("error");
